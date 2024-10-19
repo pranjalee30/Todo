fe1a48a55fa7d6592e9c749a872f86aadb583051
Pranjal Kumar <pranjalk449@gmail.com>
2024-10-19 15:09:58 +0530

    updated some details

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState,useEffect } from 'react'
 import {TodoProvider} from './context/index'
-import './App.css'
 import TodoForm from './components/TodoForm'
 import TodoItem from './components/TodoItem'
 
